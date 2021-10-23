ec5032daf9e90e6fd2c28428f5537db4b967b0cf
Егор Тихонов <gregor18rus@gmail.com>
2021-10-23 20:36:42 +0400

    added cron-sheduled template of service (without integration logic)

---
+++ Integrations/SteamIntegrationService/SteamIntegrationService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SteamIntegrationService
+{
+    public class SteamIntegrationService : CronJobService
+    {
+        private readonly ILogger<SteamIntegrationService> _logger;
+
+        public SteamIntegrationService(IScheduleConfig<SteamIntegrationService> config, ILogger<SteamIntegrationService> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("IntegrationService Started.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss}.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("IntegrationService Finished.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
