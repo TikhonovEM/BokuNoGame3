ba1c77006e866ae4319c9b92863f4066753d44ab
Егор Тихонов <gregor18rus@gmail.com>
2021-10-23 10:57:33 +0400

    added base CRUD controller

---
+++ GamesAPI/Controllers/BaseCRUDController.cs
@@ -0,0 +1,67 @@
+﻿using GamesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GamesAPI.Controllers
+{
+    public abstract class BaseCRUDController<TController, TModel> : ControllerBase 
+        where TController : BaseCRUDController<TController, TModel>
+        where TModel : class
+    {
+        private AppDBContext _context;
+        private ILogger<TController> _logger;
+        
+
+        protected AppDBContext Context => _context ??= HttpContext.RequestServices.GetService<AppDBContext>();
+        protected ILogger<TController> Logger => _logger ??= HttpContext.RequestServices.GetService<ILogger<TController>>();
+
+        // GET api/<GameController>/5
+        [HttpGet("{id}")]
+        public async Task<object> Get(int id)
+        {
+            return await this.Context.FindAsync<TModel>(id);
+        }
+
+        // POST api/<GameController>
+        [HttpPost]
+        public async Task<StatusCodeResult> Post([FromBody] TModel model)
+        {
+            try
+            {
+                await this.Context.AddAsync<TModel>(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Can't add to DB. Exception message: {ex.Message}");
+                return StatusCode(400);
+            }
+            return StatusCode(200);
+        }
+
+        // PUT api/<GameController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GameController>/5
+        [HttpDelete("{id}")]
+        public async Task<StatusCodeResult> Delete(int id)
+        {
+            var model = await this.Context.FindAsync<TModel>(id);
+            if (model != null)
+            {
+                _context.Remove<TModel>(model);
+                return StatusCode(200);
+            }
+            else
+                return StatusCode(413);
+        }
+
+    }
+}
--- GamesAPI/Controllers/GameController.cs
+++ GamesAPI/Controllers/GameController.cs
@@ -10,59 +10,8 @@ namespace GamesAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GameController : ControllerBase
+    public class GameController : BaseCRUDController<GameController, Game>
     {
-        private readonly AppDBContext _context;
-        private readonly ILogger<GameController> _logger;
 
-        public GameController(AppDBContext context, ILogger<GameController> logger)
-        {
-            _context = context;
-            _logger = logger;
-        }
-
-
-        // GET api/<GameController>/5
-        [HttpGet("{id}")]
-        public async Task<object> Get(int id)
-        {
-            return await _context.Games.FindAsync(id);
-        }
-
-        // POST api/<GameController>
-        [HttpPost]
-        public async Task<StatusCodeResult> Post([FromBody] Game game)
-        {
-            try
-            {
-                await _context.Games.AddAsync(game);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Can't add game to DB. Exception message: {ex.Message}");
-                return StatusCode(400);
-            }
-            return StatusCode(200);
-        }
-
-        // PUT api/<GameController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<GameController>/5
-        [HttpDelete("{id}")]
-        public async Task<StatusCodeResult> Delete(int id)
-        {
-            var game = await _context.Games.FindAsync(id);
-            if (game != null)
-            {
-                _context.Games.Remove(game);
-                return StatusCode(200);
-            }
-            else
-                return StatusCode(413);
-        }
     }
 }
