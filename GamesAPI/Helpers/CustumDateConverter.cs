04290658f90a4c73a1eaadc3ecab7b5a1dd22bf6
Егор Тихонов <gregor18rus@gmail.com>
2021-12-03 20:00:14 +0400

    заготовка парсера (пока без шаблонов)

---
+++ GamesAPI/Helpers/CustumDateConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Bng.Bng.GamesAPI.Helpers
+{
+    public static class CustumDateConverter
+    {
+        private static List<string> _formats = new List<string>()
+        {
+            ""
+        };
+        public static DateTime Convert(string customDate)
+        {
+            if (DateTime.TryParseExact(customDate, _formats.ToArray(), CultureInfo.InvariantCulture, DateTimeStyles.None, out var convertedDate))
+                return convertedDate;
+            return default;
+        }
+    }
+}
