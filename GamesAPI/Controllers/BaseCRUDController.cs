2534ebe8dee12362443fbd6ea277f35093137421
Егор Тихонов <gregor18rus@gmail.com>
2021-12-04 14:09:35 +0400

    Реализован PUT метод crud-api

--- GamesAPI/Controllers/BaseCRUDController.cs
+++ GamesAPI/Controllers/BaseCRUDController.cs
@@ -10,13 +10,13 @@ using Bng.GamesAPI.Contexts;
 
 namespace Bng.GamesAPI.Controllers
 {
-    public abstract class BaseCRUDController<TController, TModel> : ControllerBase 
+    public abstract class BaseCRUDController<TController, TModel> : ControllerBase
         where TController : BaseCRUDController<TController, TModel>
         where TModel : class, IEntity
     {
         private AppDBContext _context;
         private ILogger<TController> _logger;
-        
+
 
         protected AppDBContext Context => _context ??= HttpContext.RequestServices.GetService<AppDBContext>();
         protected ILogger<TController> Logger => _logger ??= HttpContext.RequestServices.GetService<ILogger<TController>>();
@@ -37,15 +37,34 @@ namespace Bng.GamesAPI.Controllers
             }
             catch (Exception ex)
             {
-                Logger.LogError($"Can't add to DB. Exception message: {ex.Message}");
-                return StatusCode(400);
+                Logger.LogError($"Can't add model(Id = {model.Id}, Type = {typeof(TModel).Name}) to DB. Exception message: {ex.Message}");
+                return StatusCode(400, ex);
             }
             return StatusCode(200, model.Id);
         }
 
         [HttpPut("{id}")]
-        public virtual void Put(int id, [FromBody] TModel model)
+        public virtual async Task<IActionResult> Put(int id, [FromBody] TModel model)
         {
+            var entity = await this.Context.FindAsync<TModel>(id);
+            if (entity != null)
+            {
+                try
+                {
+                    this.Context.Entry<TModel>(entity).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+                    entity = model;
+                    entity.Id = id;
+                    this.Context.Update<TModel>(entity);
+                    await this.Context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"Can't update model(Id = {model.Id}, Type = {typeof(TModel).Name}) to DB. Exception message: {ex.Message}");
+                    return StatusCode(400, ex);
+                }
+                return StatusCode(200, entity.Id);
+            }
+            return StatusCode(404, $"Entity with Id = {model.Id} not found");
         }
 
         [HttpDelete("{id}")]
