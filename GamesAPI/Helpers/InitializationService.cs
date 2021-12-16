9acc83f76261d63083701dd1d73f6c97cc4f8f5c
Егор Тихонов <gregor18rus@gmail.com>
2021-12-16 16:48:48 +0400

    added initialization and fixed some namespace bugs in GamesAPI

---
+++ GamesAPI/Helpers/InitializationService.cs
@@ -0,0 +1,35 @@
+﻿using Bng.GamesAPI.Contexts;
+using Bng.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bng.GamesAPI.Helpers
+{
+    public static class InitializationService
+    {
+        public static async Task InitializeAsync(AppDBContext context)
+        {
+            var catalogs = new List<string>()
+            {
+                "Запланировано",
+                "Играю на данный момент",
+                "Пройдено",
+                "Брошено"
+            };
+            foreach (var catalogName in catalogs)
+            {
+                if (!context.Catalogs.Any(c => c.Name.Equals(catalogName)))
+                {
+                    var catalog = new Catalog()
+                    {
+                        Name = catalogName
+                    };
+                    context.Catalogs.Add(catalog);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
